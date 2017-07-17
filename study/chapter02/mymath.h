75e3e09f079bc9fedcd15dff6671604bee9854ae
lhrimperial <lhr9563215@163.com>
2017-07-17 08:01:01 -0700

    commit

---
+++ study/chapter02/mymath.h
@@ -0,0 +1,7 @@
+#ifndef __MYMATH_H__
+#define __MYMATH_H__
+
+int add(int x, int y);
+
+
+#endif
---
+++ study/chapter02/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "mymath.h"
+
+int main(){
+    printf("add result: %d\n", add(1, 2));
+    return 0;
+}
+
