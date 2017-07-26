a0c08da16566223ea0a1c0954419ffacd95fd0d8
lhrimperial <lhr9563215@163.com>
2017-07-26 10:52:19 -0700

    commit

--- study/chapter05/ptest1.c
+++ study/chapter05/ptest1.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
-    printf("%c  %c\n", *++argv[0], (*++argv)[0]);
+//  printf("%c\n", *++argv[0]);
+  printf("%c\n", (*++argv)[0]);
+  printf("%c\n", *++argv[0]);
+   // printf("%c %c\n", *++argv[0], (*++argv)[0]);
 }
