6d9b5bf7cf28d0870f74a7b153cab8f628e6e320
lhrimperial <lhr9563215@163.com>
2017-07-27 11:06:07 -0700

    commit

---
+++ study/chapter06/count_keyword.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct key{
+    char *word;
+    int count;
+};
+
+struct key keytab[] = {
+    
+}
+{"int",0},
+{"char",0},
+{"float",0},
+{"double",0},
+{"long",0},
+{"for",0},
+{"while",0},
+{"switch",0},
+{"break",0},
+{"contine",0},
+{"case",0},
+{"default",0},
+{"void",0}
